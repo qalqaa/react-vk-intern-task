862305768f61de25db75f19cd2a5ecf5027f262a
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-14 13:37:19 +0300

    feat: added prime react lib
    Использовал именно primereact, т.к чаще всего работал с компонентными фреймворками от prime(PrimeNG, PrimeReact, PrimeVue), также их компоненты выглядят довольно ванильно, при этом имея неплохой простор для кастомизации

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import './App.css'
+import styles from './App.module.css';
+import { Button } from 'primereact/button';
+import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
 
 function App() {
-
   return (
     <>
+      <div className={styles.container}>
+        <Button label="Submit" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { PrimeReactProvider } from 'primereact/api';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PrimeReactProvider>
+      <App />
+    </PrimeReactProvider>
   </StrictMode>,
-)
+);
