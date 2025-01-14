330bef7b3a9cde7bef46adeaca35ade1d8e44e97
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-14 21:55:59 +0300

    feat: added debounce time to stop ddos github :)

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import styles from './App.module.scss';
-import { Toast, ToastMessage } from 'primereact/toast';
+import React, { useRef, useEffect } from 'react';
+import { debounce } from 'lodash';
 import { observer } from 'mobx-react-lite';
 import store from './stores/store';
-import { RefObject, useEffect, useRef } from 'react';
+import styles from './App.module.scss';
+import { Toast, ToastMessage } from 'primereact/toast';
 import { Skeleton } from 'primereact/skeleton';
 import Card from './components/Card/Card';
 import Header from './components/Header/Header';
@@ -11,6 +12,17 @@ const App = observer(() => {
   const listRef = useRef<HTMLDivElement>(null);
   const toastRef = useRef<Toast>(null);
 
+  const debouncedHandleScroll = debounce(() => {
+    if (listRef.current) {
+      const { scrollHeight, scrollTop, clientHeight } = listRef.current;
+      const bottom = scrollHeight === scrollTop + clientHeight;
+
+      if (bottom && !store.loading) {
+        store.fetchItems();
+      }
+    }
+  }, 200);
+
   useEffect(() => {
     if (!store.items.length && !store.loading) {
       store.fetchItems();
@@ -18,38 +30,42 @@ const App = observer(() => {
   }, []);
 
   useEffect(() => {
-    useToast('error', store.error, toastRef);
-  }, [store.error]);
+    const ref = listRef.current;
+    if (ref) {
+      ref.addEventListener('scroll', debouncedHandleScroll);
+    }
+
+    return () => {
+      if (ref) {
+        ref.removeEventListener('scroll', debouncedHandleScroll);
+      }
+      debouncedHandleScroll.cancel();
+    };
+  }, [debouncedHandleScroll]);
 
   const useToast = (
     severity: ToastMessage['severity'],
     state: string | null,
-    ref: RefObject<Toast | null>,
+    ref: React.RefObject<Toast | null>,
   ) => {
     if (state && ref.current && severity) {
       ref.current.show({
         severity: severity,
-        summary: severity.slice(0, 1).toUpperCase() + severity.slice(1),
+        summary: severity.charAt(0).toUpperCase() + severity.slice(1),
         detail: state,
       });
     }
   };
 
-  const handleScroll = () => {
-    if (listRef.current) {
-      const { scrollHeight, scrollTop, clientHeight } = listRef.current;
-      const bottom = scrollHeight === scrollTop + clientHeight;
-      if (bottom && !store.loading) {
-        store.fetchItems();
-      }
-    }
-  };
+  useEffect(() => {
+    useToast('error', store.error, toastRef);
+  }, [store.error]);
 
   return (
     <>
       <Header />
       <Toast ref={toastRef} />
-      <div ref={listRef} onScroll={handleScroll} className={styles.container}>
+      <div ref={listRef} className={styles.container}>
         <ul className={styles.list}>
           {store.items.map((item) => (
             <Card
