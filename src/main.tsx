8c29a2cc48eb898f85a51cd728ae834d48a6ca3a
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-14 13:40:44 +0300

    sm-feat: added some beauty

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import './index.css';
 import { PrimeReactProvider } from 'primereact/api';
 import App from './App.tsx';
 
+const primeStyles = {
+  ripple: true,
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PrimeReactProvider>
+    <PrimeReactProvider value={primeStyles}>
       <App />
     </PrimeReactProvider>
   </StrictMode>,
