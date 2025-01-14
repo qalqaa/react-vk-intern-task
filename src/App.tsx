a6dfc6863afdc9a4fb72552d78a5a75b7bc13658
qalqaa <113594321+qalqaa@users.noreply.github.com>
2025-01-14 13:57:55 +0300

    dev(test): some MobX store strict tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import styles from './App.module.css';
 import { Button } from 'primereact/button';
 import 'primereact/resources/themes/bootstrap4-light-blue/theme.css';
+import { observer } from 'mobx-react-lite';
+import store from './stores/store';
 
-function App() {
+const App = observer(() => {
   return (
     <>
       <div className={styles.container}>
-        <Button label="Submit" />
+        <span>Cont:{store.count}</span>
+        <Button onClick={() => store.decrement(1)} label="-" />
+        <Button onClick={() => store.increment(1)} label="+" />
       </div>
     </>
   );
-}
+});
 
 export default App;
